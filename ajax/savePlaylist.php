e6870e789d9fe3b78d4b290a9d46b219ed72e288
Yoggi Firmanda <y.firmanda@netpoint-media.de>
2015-12-15 09:57:49 +0100

    date of save playlist

--- ajax/savePlaylist.php
+++ ajax/savePlaylist.php
@@ -6,6 +6,7 @@ $db = new Storage();
 $_GET['daten'] = urldecode(stripslashes($_GET['daten']));
 $daten['lists'] = $_GET['daten'];
 $daten['name'] = $_GET['name'];
+$daten['created_at'] = date("Y-m-d H:i:s");
 $db->insert('playlist',$daten);
 
 ?>
