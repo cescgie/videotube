94f91dcfdea912e85f65d6429859e35527e21e2a
Yoggi Firmanda <y.firmanda@netpoint-media.de>
2016-01-07 16:47:13 +0100

    optimize search and show views each video

--- ajax/getLeftVideo.php
+++ ajax/getLeftVideo.php
@@ -5,7 +5,7 @@ $db = new Storage();
 $q = '%';
 if(isset($_GET['q']) && $_GET['q'] != ''){
   $key = $_GET['q'];
-  $result = $db->select("SELECT * FROM yapi WHERE title IS NOT NULL AND title != '' AND LOWER(title) LIKE LOWER('%$key%') LIMIT 25");
+  $result = $db->select("SELECT * FROM yapi WHERE title IS NOT NULL AND title != '' AND BINARY LOWER(title) LIKE LOWER('%$key%') OR REPLACE(title, ' ', '') LIKE LOWER('%$key%') LIMIT 25");
 }else{
   $key = '%';
   $result = $db->select("SELECT * FROM yapi WHERE title IS NOT NULL AND title != '' ORDER BY id DESC LIMIT 25");
@@ -15,7 +15,7 @@ foreach ($result as $keys => $value) {
   echo
    '<div id="leftVideo'.$i.'" more_id="'.$i.'" key="'.$key.'" class="video" onclick="video_click('.$i.')" video_yid="'.$value["yid"].'" video_title="'.$value["title"].'">
     <img class="video-image" src="'.$value['thumbnail'].'">
-    <p class="video-title">'.$value['title'].'</p>
+    <p class="video-title">'.$value['title'].' ('.$value['viewers'].' Views)</p>
     <p class="video-author">'.$value['author'].'</p>
     <p class="video-description">'.$value['description'].'</p>
     </div>
