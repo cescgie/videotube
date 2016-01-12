c914108c3b50d409e75ef39d4b9d71324804d647
Yoggi Firmanda <y.firmanda@netpoint-media.de>
2016-01-12 12:22:39 +0100

    special char bug by get playlist fixed

--- ajax/getPlaylist.php
+++ ajax/getPlaylist.php
@@ -1,18 +1,9 @@
 <?php
-require_once('../libs/db.php');
+require_once('../libs/Storage.php');
 
-//$key = "hamburg";
+$db = new Storage();
 $key = strtolower($_GET['name']);
 
-$query="SELECT lists FROM playlist WHERE LOWER(name) LIKE LOWER('$key') ";
-
-$result = $mysqli->query($query) or die($mysqli->error.__LINE__);
-
-$arr = array();
-if($result->num_rows > 0) {
-	while($row = $result->fetch_assoc()) {
-		$arr[] = $row;
-	}
-}
-print_r($arr[0]['lists']);
+$result = $db->select("SELECT lists FROM playlist WHERE LOWER(name) LIKE LOWER('$key') ");
+print_r($result[0]['lists']);
 ?>
